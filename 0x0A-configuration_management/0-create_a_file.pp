c43b34d58aa185a5abfeb265d5eabdbeb7d46ecb
Safuan04 <elmail.safuan@gmail.com>
2023-09-22 16:09:52 +0100

    create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,6 @@
 file {'/tmp/school':
-    ensure  => present,
-    path    => '/tmp/school'
-    mode    => '0744',
-    owner   => 'www-data',
-    group   => 'www-data',
-    content => 'I love Puppet',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744'
 }
